ea8a144162ca9ca5c14b10c2fdbda677e5f04fdb
Kaushikj14 <kaushikjain1423@gmail.com>
2024-09-17 00:23:50 +0530

    Crons Added in project

--- src/main/java/com/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/com/engineeringdigest/journalApp/JournalApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableTransactionManagement // (when you are applying the transaction annotation on any method you have to also use this annotation so operations can perform)
-
+@EnableScheduling
 public class JournalApplication {
 
 	public static void main(String[] args) {
